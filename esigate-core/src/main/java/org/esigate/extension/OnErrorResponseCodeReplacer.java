a7ab7d25d350fe143663aad100d01d5fb202edd1
VeekeeFr <20480977+VeekeeFr@users.noreply.github.com>
2022-09-13 13:23:09 +0200

    [responseCode] Switched to status code mapping (using regex)

--- esigate-core/src/main/java/org/esigate/extension/OnErrorResponseCodeReplacer.java
+++ esigate-core/src/main/java/org/esigate/extension/OnErrorResponseCodeReplacer.java
@@ -15,7 +15,11 @@
 
 package org.esigate.extension;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 import org.apache.http.HttpStatus;
 import org.esigate.Driver;
@@ -25,41 +29,54 @@ import org.esigate.events.EventManager;
 import org.esigate.events.IEventListener;
 import org.esigate.events.impl.FragmentEvent;
 import org.esigate.util.Parameter;
-import org.esigate.util.ParameterInteger;
+import org.esigate.util.ParameterCollection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * This extension replaces response code when an error was returned during process
  * <p>
- * Be sure to put this extension last
+ * Be sure to put this extension last.
  * 
  * <p>
  * Variables:
  * <ul>
- * <li>onerror_response_code: integer (response code used as replacement)</li>
+ * <li>onerror_response_code: pattern:int (response code used as replacement)</li>
  * </ul>
  * </p>
+ * 
+ * Note: Special response code value '0' means that the response code will be unchanged.
  * 
  * @author VeekeeFr
  * 
  */
 public class OnErrorResponseCodeReplacer implements Extension, IEventListener {
     private static final Logger LOG = LoggerFactory.getLogger(OnErrorResponseCodeReplacer.class);
-    public static final Parameter<Integer> ON_ERROR_RESPONSE_CODE = new ParameterInteger("onerror_response_code", 0);
-    private int newResponseCode;
+    public static final Parameter<Collection<String>> ON_ERROR_RESPONSE_CODE = new ParameterCollection(
+            "onerror_response_code");
+    private Driver driver;
+    private List<OnErrorResponseCodeReplacerMatcher> responseCodeMapping;
 
     @Override
     public void init(Driver pDriver, Properties properties) {
-        this.newResponseCode = ON_ERROR_RESPONSE_CODE.getValue(properties);
-        if (newResponseCode == 0) {
+        this.driver = pDriver;
+		this.responseCodeMapping = new ArrayList<>();
+		ON_ERROR_RESPONSE_CODE.getValue(properties)
+				.forEach(value -> {
+					String[] kv = value.split(":", 2);
+					this.responseCodeMapping.add(new OnErrorResponseCodeReplacerMatcher(kv[0], Integer.parseInt(kv[1])));
+				});
+        if (this.responseCodeMapping.isEmpty()) {
             LOG.warn(
-                    "No onerror_response_code value could be retrieved for instance '{}'... Extension will be disabled!",
-                    pDriver.getConfiguration().getInstanceName());
+                    "No onerror_response_code mapping could be retrieved for instance '{}'... Extension will be disabled!",
+                    this.driver.getConfiguration().getInstanceName());
             return;
+        } else {
+            LOG.warn("Enabling onerror_response_code extension for instance '{}'", this.driver.getConfiguration()
+                    .getInstanceName());
         }
 
-		pDriver.getEventManager().register(EventManager.EVENT_FRAGMENT_POST, this);
+        pDriver.getEventManager().register(EventManager.EVENT_FRAGMENT_POST, this);
     }
 
     @Override
@@ -68,12 +85,43 @@ public class OnErrorResponseCodeReplacer implements Extension, IEventListener {
 
         int statusCode = e.getHttpResponse().getStatusLine().getStatusCode();
 
+		if(LOG.isDebugEnabled()) {
+			LOG.debug("Return code '{}' detected for '{}' (instance '{}')...", statusCode, e.getHttpRequest().getBaseUrl(),
+					this.driver.getConfiguration().getInstanceName());
+		}
         if (statusCode >= HttpStatus.SC_BAD_REQUEST) {
+			String sStatusCode = String.valueOf(statusCode);
+			int newResponseCode = this.responseCodeMapping
+					.stream()
+					.filter(rule -> rule.matches(sStatusCode))
+					.findFirst()
+					.map(rule -> rule.responseCode)
+					.orElseGet(() -> 0);
+			if(newResponseCode <= 0) {
+				newResponseCode = statusCode;
+			}
+
+			if(LOG.isDebugEnabled()) {
+				LOG.debug("Replacing with '{}'!", newResponseCode);
+			}
             e.getHttpResponse().setStatusCode(newResponseCode);
         }
 
         // Continue processing
         return true;
     }
+}
 
+class OnErrorResponseCodeReplacerMatcher {
+    private final Pattern regex;
+    public final int responseCode;
+
+    public OnErrorResponseCodeReplacerMatcher(String rgx, int rc) {
+        this.regex = Pattern.compile(rgx);
+        this.responseCode = rc;
+    }
+
+    public boolean matches(String value) {
+        return this.regex.matcher(value).matches();
+    }
 }
